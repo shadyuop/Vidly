c8491d9ca0b7cfd56c4f40519f5e392d7b97810d
es-ShadiSaeed2020@alexu.edu.eg <es-ShadiSaeed2020@alexu.edu.eg>
2020-08-17 00:15:49 +0200

    Adding Birthdate to a Customer

--- Models/Customer.cs
+++ Models/Customer.cs
@@ -17,5 +17,6 @@ namespace VidlyT.Models
         public bool IsSubscribedToNewsLetter { get; set; }
         public MembershipType MembershipType { get; set; }
         public byte MembershipTypeId { get; set; }
+        public DateTime? BirthDate { get; set; }
     }
 }
