e2d5a291392668d155481316bb4064e732cf4bf1
es-ShadiSaeed2020@alexu.edu.eg <es-ShadiSaeed2020@alexu.edu.eg>
2020-08-17 04:37:22 +0200

    Editing Customers through Form

--- Controllers/CustomersController.cs
+++ Controllers/CustomersController.cs
@@ -3,6 +3,7 @@ using System.Linq;
 using System.Data.Entity;
 using System.Web.Mvc;
 using VidlyT.Models;
+using VidlyT.ViewModels;
 
 namespace Vidly.Controllers
 {
@@ -22,8 +23,22 @@ namespace Vidly.Controllers
 
         public ActionResult New()
         {
+            var membershipTypes = _context.MembershipTypes.ToList();
+            var viewModel = new CustomerFormViewModel
+            {
+                MembershipTypes = membershipTypes,
+            };
+            
+            return View("CustomerForm",viewModel);
+        }
 
-            return View();
+        [HttpPost]
+        public ActionResult Create(Customer customer)
+        {
+            _context.Customers.Add(customer);
+            _context.SaveChanges();
+
+            return RedirectToAction("Index", "Customers");
         }
 
         public ViewResult Index()
@@ -43,5 +58,21 @@ namespace Vidly.Controllers
             return View(customer);
         }
 
+        public ActionResult Edit(int id)
+        {
+            var customer = _context.Customers.SingleOrDefault(c => c.Id == id);
+
+            if (customer == null)
+            {
+                return HttpNotFound();
+            }
+
+            var viewModel = new CustomerFormViewModel
+            {
+                Customer = customer,
+                MembershipTypes = _context.MembershipTypes.ToList()
+            };
+            return View("CustomerForm", viewModel);
+        }   
     }
 }
--- Models/Customer.cs
+++ Models/Customer.cs
@@ -14,9 +14,14 @@ namespace VidlyT.Models
         [Required]
         [StringLength(255)]
         public string Name { get; set; }
+        
         public bool IsSubscribedToNewsLetter { get; set; }
+
         public MembershipType MembershipType { get; set; }
+        
         public byte MembershipTypeId { get; set; }
+        
+        [Display(Name = "Date of Birth")]
         public DateTime? BirthDate { get; set; }
     }
 }
